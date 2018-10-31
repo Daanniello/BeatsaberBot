2b318ce283b7629610cc25a1fd4095985ed1554f
Daanniello <daansmits_@hotmail.com>
2018-10-31 22:55:35 +0100

    Compare

--- DiscordBeatSaberBot/DiscordBeatSaberBot/BeatSaberInfoExtension.cs
+++ DiscordBeatSaberBot/DiscordBeatSaberBot/BeatSaberInfoExtension.cs
@@ -778,5 +778,50 @@ namespace DiscordBeatSaberBot
 
             return players;
         }
+
+        public static async Task<EmbedBuilder> PlayerComparer(string message)
+        {
+            var splitting = message.Split(" vs ");
+
+            var player1List = await GetPlayerInfo(splitting[0]);
+            var player2List = await GetPlayerInfo(splitting[1]);
+
+            var player1 = player1List.First();
+            var player2 = player2List.First();
+
+            var builder = new EmbedBuilder();
+
+            //builder.AddInlineField(splitting[0],
+            //    "Rank" + "\n" +
+            //    "Country" + "\n" +
+            //    "CountryRank" + "\n" +
+            //    "PlayCount" + "\n" +
+            //    "PP" + "\n" +
+            //    "TotalScore" + "\n");
+
+            builder.AddInlineField(splitting[0] + " " + player1.countryIcon.ToLower(), 
+                "#"+ player1.rank + "\n" +
+                "#" + player1.countryRank + "\n" +
+                player1.playCount + "\n" +
+                player1.pp + "\n" +
+                player1.totalScore + "\n");
+
+            builder.AddInlineField("Difference",
+                 (Math.Abs(player1.rank - player2.rank)) + "\n" +
+                 (Math.Abs(player1.countryRank - player2.countryRank)) + "\n" +
+                 (Math.Abs(player1.playCount - player2.playCount)) + "\n" +
+                 (player1.pp + player2.pp) + "\n" +
+                 (Math.Abs((int.Parse(player1.totalScore.Replace(",","")) - int.Parse(player2.totalScore.Replace(",", "")))) + "\n"));
+
+            builder.AddInlineField(splitting[1] + " " + player2.countryIcon.ToLower(),
+                "#" + player2.rank + "\n" +
+                "#" + player2.countryRank + "\n" +
+                player2.playCount + "\n" +
+                player2.pp + "\n" +
+                player2.totalScore + "\n");
+
+            
+            return builder;
+        } 
     }
 }
