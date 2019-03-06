1906e30f5a4a87ab8916c3d989facbeafe98af24
Daanniello <daansmits_@hotmail.com>
2019-03-06 17:36:58 +0100

    danskbog

---
+++ DiscordBeatSaberBot/DiscordBeatSaberBot/Danskbog.cs
@@ -0,0 +1,104 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiscordBeatSaberBot
+{
+    class Danskbog
+    {
+        private List<TimeSpan> schedule;
+        private List<string> scheduleMessage;
+        private TimeSpan timeNow;
+        private DiscordSocketClient discord;
+
+        public Danskbog(DiscordSocketClient discord)
+        {
+            this.discord = discord;
+            timeNow = new TimeSpan(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Minute);
+            var ontbijt = new TimeSpan(10, 00, 00);
+            var middag = new TimeSpan(12, 00, 00);
+            var voorAvond = new TimeSpan(15, 00, 00);
+            var avond = new TimeSpan(19, 00, 00);
+            var nacht = new TimeSpan(21, 00, 00);
+            var slapen = new TimeSpan(23, 00, 00);
+
+            var mention = "<@221373638979485696> <@138439306774577152>";
+            var ontbijtMessage = "**10:00** - Goeiemorgen <3, Je moet ontbijten ^^ Eet smakelijk " + mention;
+            var middagMessage = "**12:00** - Woaa het is middag, vergeet niet een tosti te nemen en wat te drinken " + mention;
+            var voorAvondMessage = "**15:00** - -w- heheh middagsnack tijd!! neem een tosti en wat fruit :3 vergeet je drankje niet! " + mention;
+            var avondMessage = "**19:00** - nomnomnom avond eten =3 eetsmakelijk " + mention;
+            var nachtMessage = "**21:00** - nog ff goed eten voor je gaat slapen ^^ neem een tosti en vergeet je fruit/drankje niet :3 " + mention;
+            var slapenMessage = "**23:00** - nacht toetje hehehe ;3 en noten woahh... slaaplekker alvast <3 " + mention;
+
+
+            schedule = new List<TimeSpan>();
+            scheduleMessage = new List<string>();
+
+            schedule.Add(ontbijt);
+            schedule.Add(middag);
+            schedule.Add(voorAvond);
+            schedule.Add(avond);
+            schedule.Add(nacht);
+            schedule.Add(slapen);
+
+            scheduleMessage.Add(ontbijtMessage);
+            scheduleMessage.Add(middagMessage);
+            scheduleMessage.Add(voorAvondMessage);
+            scheduleMessage.Add(avondMessage);
+            scheduleMessage.Add(nachtMessage);
+            scheduleMessage.Add(slapenMessage);
+
+
+
+            TimerRunning(new CancellationToken());
+
+
+        }
+
+        private async void TimerRunning(CancellationToken token)
+        {
+            var watch = Stopwatch.StartNew();
+            while (!token.IsCancellationRequested)
+            {
+                try
+                {
+                    var sleep = GetNextMeal();
+                    Console.WriteLine("Waiting for next food time for danskbog <3...");
+                    await Task.Delay((int)(sleep.Item1.Negate().TotalMilliseconds) - (int)(watch.ElapsedMilliseconds % 1000), token);
+                    ISocketMessageChannel channel = (ISocketMessageChannel)discord.GetGuild(439514151040057344).GetChannel(552874394255360008);
+                    var guild = discord.GetGuild(439514151040057344);
+                    var channelid = guild.GetTextChannel(552874394255360008);
+                    await channelid.SendMessageAsync(scheduleMessage[sleep.Item2]);
+
+                    Console.WriteLine("Food Time!");
+                }
+                catch (TaskCanceledException)
+                {
+
+                }
+            }
+        }
+
+        private (TimeSpan, int) GetNextMeal()
+        {
+            var timeNow = new TimeSpan(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Minute);
+            TimeSpan temp = new TimeSpan();
+            var count = 0;
+            foreach (var mealtime in schedule)
+            {
+                var value = timeNow.Subtract(mealtime);
+                if (temp > value)
+                {
+                    return (value, count);
+                }
+                count++;
+            }
+            return (new TimeSpan(), -1);
+
+        }
+    }
+}
